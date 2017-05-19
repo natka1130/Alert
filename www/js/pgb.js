e8fe556c10a362403c406b7f9a8dc15361241875
Natalia Lewczuk <Natalia Lewczuk>
2017-05-19 23:33:39 +0200

    lala

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -6,19 +6,6 @@ function onDeviceReady() {
 	navigator.notification.beep(1);
 }
 
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
 }
 
 function personalInfo(){
